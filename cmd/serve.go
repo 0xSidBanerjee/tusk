68223d7cd791c1ff1e7a75d8ea436e64ee672d76
0xSidBanerjee <siddhantban02@gmail.com>
2026-05-09 20:41:32 +0530

    feat(cli): add serve command with db-file, address, port and open-browser flags

--- cmd/serve.go
+++ cmd/serve.go
@@ -9,31 +9,29 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	dbFile      string
+	address     string
+	port        int
+	openBrowser bool
+)
+
 // serveCmd represents the serve command
 var serveCmd = &cobra.Command{
 	Use:   "serve",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Start the todo web application",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("serve called")
+		fmt.Printf("Starting server on %s:%d\n", address, port)
+		fmt.Printf("Database file: %s\n", dbFile)
+		fmt.Printf("Open browser: %v\n", openBrowser)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(serveCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// serveCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// serveCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	serveCmd.Flags().StringVarP(&dbFile, "db-file", "d", "todo.db", "The path to sqlite database file to use")
+	serveCmd.Flags().StringVarP(&address, "address", "b", "localhost", "The ip address to bind to")
+	serveCmd.Flags().IntVarP(&port, "port", "p", 8080, "The port to listen on")
+	serveCmd.Flags().BoolVarP(&openBrowser, "open-browser", "o", true, "Specify if app automatically opens in browser")
 }
